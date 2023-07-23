1ab4809d6c76524004fb6caf723a0777ae068cb1
Javier Vite Ramirez <javier-051@outlook.es>
2023-07-22 22:02:56 -0600

    Agregar archivos de proyecto.

---
+++ Movies/Client/Utilities/Utilities.cs
@@ -0,0 +1,10 @@
+﻿namespace Movies.Client.Utilities
+{
+    public class Utilities
+    {
+        public static string transformLowerCase(string name)
+        {
+            return name.ToLower();
+        }
+    }
+}
